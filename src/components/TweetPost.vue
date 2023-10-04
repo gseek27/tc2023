c86d0cdc97c54a609a0ba6621ef00c75b42af7d3
Kharel <goalseek27@gmail.com>
2023-10-04 00:24:44 -0500

    commit6

--- src/components/TweetPost.vue
+++ src/components/TweetPost.vue
@@ -1,28 +1,73 @@
 <template>
-<div v-if="tweetData.content" class="tweet-post">
+  <div v-if="tweetData.content" class="tweet-post">
     <v-avatar v-if="tweetData.profileImage">
-          <img :src="tweetData.profileImage">
+      <img :src="tweetData.profileImage">
     </v-avatar>
     <strong v-if="tweetData.username">@{{ tweetData.username }}</strong>
     <p v-if="tweetData.content">{{ tweetData.content }}</p>
-    <v-img v-if="tweetData.imageUrl" :src="tweetData.imageUrl" aspect-ratio="2"> </v-img>  
-    <button v-if="tweetData.content" @click="deleteTweet">Delete</button>     
+
+    <!-- Display media based on its type -->
+    <v-img v-if="isImage(tweetData.imageUrl) || isGif(tweetData.imageUrl)" :src="tweetData.imageUrl" aspect-ratio="2"></v-img>
+  
+   <!--<video v-if="videoURL" controls preload="auto" playsinline> -->
+    <!-- <source :src="videoURL" type="video/mp4"> -->
+  <!-- Display Video -->
+    <video v-if="isVideo(tweetData.imageUrl)" controls preload="auto" playsinline>
+      <source :src="tweetData.imageUrl" type="video/mp4">
+      Your browser does not support the video tag.
+    </video>
+
+    <button v-if="tweetData.content" @click="deleteTweet">Delete</button>
   </div>
 </template>
 
 <script>
 import { getFirestore, doc, deleteDoc } from 'firebase/firestore';
+import { getStorage, ref as storageRef, getDownloadURL } from 'firebase/storage';
 
 export default {
   props: {
     tweetData: {
       type: Object,
       required: true,
-     default: () => ({})  // Default is now an empty object
+      default: () => ({})  // Default is now an empty object
     }
-  }
-  ,
+  },
+  data() {
+    return {
+      videoURL: '' // This will be set after fetching from Firebase
+    };
+  },
+  mounted() {
+    // Assuming `tweetData.imageUrl` contains the path to the video in Firebase Storage
+    const storagePath = this.tweetData.imageUrl;
+    
+    if (storagePath) {
+      const storage = getStorage();
+      const videoRef = storageRef(storage, storagePath);
+
+      getDownloadURL(videoRef)
+        .then((url) => {
+          this.videoURL = url;
+        })
+        .catch((error) => {
+          console.error("Error fetching video URL:", error);
+        });
+    }
+  },
   methods: {
+    isImage(url) {
+      // Updated to detect .jpeg, .jpg, .png anywhere in the URL string
+      return /.*\.(jpeg|jpg|png)/i.test(url);
+    },
+      isGif(url) {
+      // Updated to detect .gif anywhere in the URL string
+      return /.*\.gif/i.test(url);
+    },
+    isVideo(url) {
+      // Updated to detect .mp4, .webm, and .ogg anywhere in the URL string
+      return /.*\.(mp4|webm|ogg)/i.test(url);
+    },
     async deleteTweet() {
       try {
         const db = getFirestore();
@@ -39,15 +84,15 @@ export default {
 
 <style scoped>
 .tweet-post {
-  margin-bottom: 90px; /* adjust as needed */
-  border: 1px solid #e0e0e0;  /* You can adjust the color and thickness as you like */
-  padding: 10px;              /* Add some padding inside the border for better appearance */
-  margin-top: 30px;        /* Add some space between posts if you have multiple posts in a list */
-  border-radius: 5px;    
+  margin-bottom: 90px;
+  border: 1px solid #e0e0e0;
+  padding: 10px;
+  margin-top: 30px;
+  border-radius: 5px;
 }
 .tweet-post button {
-  display: block; 
-  background-color: red; 
-  color: white;   
+  display: block;
+  background-color: red;
+  color: white;
 }
 </style>
