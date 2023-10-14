5a56507089f2f4571ca8e1b1cdbe816949cacf6c
Kharel <goalseek27@gmail.com>
2023-10-14 18:04:20 -0500

    update3

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e9eacd9013b5c4ce3a71e8d1d84fbb78ebe79a68855a6de54d3a16670bbf8e0
-size 73
+oid sha256:9feea7b6e1ad7ff0172d46be493daab3a742793fbb36b35f9acb531dda2599bb
+size 83
