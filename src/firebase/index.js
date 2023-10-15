04adccdd430ba5d064039a6fe7029aa5a6c0c5ec
Kharel <goalseek27@gmail.com>
2023-10-15 10:02:55 -0500

    wed18

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,3 +1,60 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bcf3d6c5e58b5f608260224ed6beeb73cb4c40e007ed3379d31143cd70e3569a
-size 1284
+// Import the functions you need from the SDKs you need
+//import firebase from 'firebase/app';
+import { initializeApp } from 'firebase/app'
+//import { getAnalytics } from "firebase/analy
+
+import { setPersistence, browserLocalPersistence } from 'firebase/auth'
+
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+
+//import { getFirestore, addDoc, collection } from 'firebase/firestore';  // <-- Import FieldValue
+//import { serverTimestamp } from 'firebase/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+const apiKey = process.env.VUE_APP_FIRESTORE_API_KEY
+const authDomain = process.env.VUE_APP_FIRESTORE_AUTH_DOMAIN
+const projectId = process.env.VUE_APP_FIRESTORE_PROJECT_ID
+const storageBucket = process.env.VUE_APP_FIRESTORE_STORAGE_BUCKET
+const messagingSenderId = process.env.VUE_APP_FIRESTORE_MESSAGING_SENDER_ID
+const appId = process.env.VUE_APP_FIRESTORE_APP_ID
+const measurementId = process.env.VUE_APP_FIRESTORE_MEASUREMENT_ID
+
+const firebaseConfig = {
+
+  apiKey: apiKey,
+
+  authDomain: authDomain,
+
+  projectId: projectId,
+
+  storageBucket: storageBucket,
+
+  messagingSenderId: messagingSenderId,
+
+  appId: appId,
+
+  measurementId: measurementId
+
+};
+
+
+
+
+const app = initializeApp(firebaseConfig)
+//const analytics = getAnalytics(app);
+
+const db = getFirestore(app)
+const auth = getAuth(app)
+
+setPersistence(auth, browserLocalPersistence)
+
+export { db, auth }
