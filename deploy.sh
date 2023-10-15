15de47c52047e85997edccad37e62eb0bd64e10b
Kharel <goalseek27@gmail.com>
2023-10-14 19:30:30 -0500

    dsfsd

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:056735c7167cca674d991bc71315b103a2a9bf9a44c88264b131366be7dac48d
-size 487
+oid sha256:5578cad26a80ce73daf0e74634f721daf017d77dbfeb4f05002dd3699798cde7
+size 488
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc2b174a5d47d91dc14e7e02eaeb78f03008b5b41dd17c599f71413e8c2117a4
-size 412
+oid sha256:ec446de44bdfee876d2b128166ed42a2190f721c07e32b7651c1dbcedeb818a9
+size 472
