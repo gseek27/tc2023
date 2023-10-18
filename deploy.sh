08b404f1f3444001c50974fd6123d432c674d859
gseek27 <goalseek27@gmail.com>
2023-10-18 07:13:59 -0500

    wed23aaaa

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-version https://git-lfs.github.com/spec/v1
-oid sha256:5578cad26a80ce73daf0e74634f721daf017d77dbfeb4f05002dd3699798cde7
-size 488
-=======
 #!/usr/bin/env sh
 
 # abort on errors
@@ -28,4 +23,3 @@ git commit -m 'deploy'
  git push -f git@github.com:gseek27/tc2023.git master:gh-pages
 
 cd -
->>>>>>> 2190573f (wed14)
